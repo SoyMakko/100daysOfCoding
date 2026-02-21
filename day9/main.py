635ac9e2be763f153a8b643ed7b1e44c8e76e6ea
SoyMakko <soymakkoo@gmail.com>
2026-02-20 20:41:05 -0600

    max function added

--- day9/main.py
+++ day9/main.py
@@ -5,11 +5,13 @@ print(logo)
 def find_highest_bidder(bidding_record):
     highest_bid = 0
     winner = ""
-    for bidder in bidding_record:
-        bid_amount = bidding_record[bidder]
-        if bid_amount > highest_bid:
-            highest_bid = bid_amount
-            winner = bidder
+    # for bidder in bidding_record:
+    #     bid_amount = bidding_record[bidder]
+    #     if bid_amount > highest_bid:
+    #         highest_bid = bid_amount
+    #         winner = bidder
+    winner = max(bidding_record, key = bidding_record.get)
+    highest_bid = bidding_record[winner]
     print(f"The winner is {winner} with a bid of ${highest_bid}")
 
 
